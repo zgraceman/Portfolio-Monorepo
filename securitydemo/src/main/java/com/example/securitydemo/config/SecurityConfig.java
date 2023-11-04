045dca8ae755daeda44583d5721229c8ade19651
zgraceman <zach.graceman@gmail.com>
2023-11-03 22:03:01 -0500

    Add configuration annotation and enhance security setup
    - Add @Configuration to SecurityConfig to enable Spring's context processing.
    - Implement UserDetailsService bean to set up in-memory user authentication.
    - Add PasswordEncoder bean to ensure passwords are securely encoded.
    - Minor refactoring and code clean-up in SecurityFilterChain bean definition.

--- securitydemo/src/main/java/com/example/securitydemo/config/SecurityConfig.java
+++ securitydemo/src/main/java/com/example/securitydemo/config/SecurityConfig.java
@@ -1,28 +1,54 @@
 package com.example.securitydemo.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 /**
  * @author zgraceman
  */
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                // Use of authorizeHttpRequests replaces authorizeRequests
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().authenticated()
                 )
-                .httpBasic(withDefaults());
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.builder()
+                .username("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER")
+                .build());
+        manager.createUser(User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("adminpass"))
+                .roles("ADMIN")
+                .build());
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
