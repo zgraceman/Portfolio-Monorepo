41ce1aa43b87a2477c0aa3d4701c3582f505f21d
zgraceman <zach.graceman@gmail.com>
2024-02-19 15:33:33 -0600

    Implemented Main and Utility classes

--- InventoryManagementSystem/InventoryManagementSystem/src/Program.cs
+++ InventoryManagementSystem/InventoryManagementSystem/src/Program.cs
@@ -3,12 +3,21 @@ using System;
 
 namespace InventoryManagementSystem
 {
-	public class Program
-	{
-		static void Main(string[] args)
-		{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Inventory inventory = new Inventory(10);
 
-		}
-	}
+            inventory.AddBook(new Book("1984", "George Orwell", 9.99m));
+            inventory.AddBook(new Book("To Kill a Mockingbird", "Harper Lee", 12.99m));
+
+            Console.WriteLine("Current Inventory:");
+            inventory.DisplayBooks();
+
+            decimal convertedPrice = Utility.ConvertCurrency(10.00m, 1.2m);
+            Console.WriteLine($"Converted Price: {convertedPrice}");
+        }
+    }
 }
 
--- InventoryManagementSystem/InventoryManagementSystem/src/Utility.cs
+++ InventoryManagementSystem/InventoryManagementSystem/src/Utility.cs
@@ -1,6 +1,13 @@
 ﻿using System;
+
 namespace InventoryManagementSystem
 {
-	public static class Utility	{}
+    public static class Utility
+    {
+        public static decimal ConvertCurrency(decimal amount, decimal exchangeRate)
+        {
+            return amount * exchangeRate;
+        }
+    }
 }
 
