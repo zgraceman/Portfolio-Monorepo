f804eba0cf8d9d6dde1e3ed4953b56114a7ee5ca
zgraceman <zach.graceman@gmail.com>
2024-02-19 15:29:09 -0600

    Implemented Inventory class

--- InventoryManagementSystem/InventoryManagementSystem/src/Inventory.cs
+++ InventoryManagementSystem/InventoryManagementSystem/src/Inventory.cs
@@ -4,6 +4,30 @@ namespace InventoryManagementSystem
 {
     public class Inventory
     {
+        private Book[] books;
+        private int count;
 
+        public Inventory(int size)
+        {
+            books = new Book[size];
+            count = 0;
+        }
+
+        public void AddBook(Book book)
+        {
+            if (count < books.Length)
+            {
+                books[count] = book;
+                count++;
+            }
+        }
+
+        public void DisplayBooks()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(books[i]);
+            }
+        }
     }
 }
