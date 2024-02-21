115a202c5fb780e37b72c8304f57308dcf1de0e3
zgraceman <zach.graceman@gmail.com>
2024-02-20 20:09:51 -0600

    Added Device Class: Extended Product class to include Device with properties like Manufacturer and Model

---
+++ InventoryManagementSystem/InventoryManagementSystem/src/Device.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace InventoryManagementSystem
+{
+    public class Device : Product
+    {
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+
+        public Device(string name, string manufacturer, string model, decimal price)
+            : base(name, price)
+        {
+            Manufacturer = manufacturer;
+            Model = model;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}, Manufacturer: {Manufacturer}, Model: {Model}";
+        }
+    }
+}
+
--- InventoryManagementSystem/InventoryManagementSystem/src/Program.cs
+++ InventoryManagementSystem/InventoryManagementSystem/src/Program.cs
@@ -11,7 +11,9 @@ namespace InventoryManagementSystem
 
             inventory.AddProduct(new Book("1984", "George Orwell", 9.99m));
             inventory.AddProduct(new Book("To Kill a Mockingbird", "Harper Lee", 12.99m));
-            // You can add other product types here as well
+            inventory.AddProduct(new Device("Zach's Smart Watch", "Garmin", "Venu 2 Plus", 200m));
+            inventory.AddProduct(new Device("Zach's Phone", "Google", "Pixel 6a", 400m));
+
 
             Console.WriteLine("Current Inventory:");
             inventory.DisplayProducts();
