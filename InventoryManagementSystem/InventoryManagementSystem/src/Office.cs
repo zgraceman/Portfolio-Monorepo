81250ad7da31197881424ceeac41a37604235414
zgraceman <zach.graceman@gmail.com>
2024-04-11 17:01:29 -0500

    Initialize office class with constructor and fields

--- InventoryManagementSystem/InventoryManagementSystem/src/Office.cs
+++ InventoryManagementSystem/InventoryManagementSystem/src/Office.cs
@@ -1,11 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace InventoryManagementSystem.src
 {
-	public class Office
+
+    public class Office
 	{
-		public Office()
-		{
-		}
-	}
+        public string Name { get; private set; }
+        public Inventory OfficeInventory { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the Office class with a specific name.
+        /// </summary>
+        /// <param name="name">The name of the office.</param>
+        public Office(string name)
+        {
+            Name = name;
+            OfficeInventory = new Inventory();
+        }
+    }
 }
 
