7e4b08218e17c49fbfaf73900e24694342f61a32
zgraceman <zach.graceman@gmail.com>
2024-04-12 10:25:12 -0500

    Add OfficeID field for improved entity tracking and future database integration

--- InventoryManagementSystem/InventoryManagementSystem/src/Office.cs
+++ InventoryManagementSystem/InventoryManagementSystem/src/Office.cs
@@ -6,15 +6,18 @@ namespace InventoryManagementSystem.src
 
     public class Office
 	{
+        public int OfficeId { get; private set; }
         public string Name { get; private set; }
         public Inventory OfficeInventory { get; private set; }
 
         /// <summary>
-        /// Initializes a new instance of the Office class with a specific name.
+        /// Initializes a new instance of the Office class with a specific name and ID.
         /// </summary>
+        /// <param name="officeId">The unique identifier for the office.</param>
         /// <param name="name">The name of the office.</param>
-        public Office(string name)
+        public Office(int officeId, string name)
         {
+            OfficeId = officeId;
             Name = name;
             OfficeInventory = new Inventory();
         }
